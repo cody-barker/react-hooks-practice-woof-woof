5c9a710b802728393d79fc3282e6b7d02310b9bc
Cody <codybarker.or@gmail.com>
2023-03-07 09:44:09 -0800

    dog button working to update gooddog

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,9 @@ import DogCard from './DogCard'
 function App() {
 
 const [pups, setPups] = useState([]);
+const [dogDetail, setDogDetail] = useState(null)
 
-const pupComps = pups.map(pup => <DogCard pup={pup}/>)
+const pupComps = pups.map(pup => <DogCard key={pup.id} pup={pup} dogDetail={dogDetail} setDogDetail={setDogDetail}/>)
 
 useEffect(() => {
   fetch('http://localhost:3001/pups')
@@ -13,18 +14,17 @@ useEffect(() => {
   .then(pups => setPups(pups))
 },[]);
 
-console.log(pups)
-
   return (
     <div className="App">
       <div id="filter-div">
         <button id="good-dog-filter">Filter good dogs: OFF</button>
       </div>
       <div id="dog-bar">{pupComps}</div>
-      
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
-        <div id="dog-info"></div>
+        <div id="dog-info">
+          {dogDetail}
+        </div>
       </div>
     </div>
   );
--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
-function DogCard({pup}) {
-    const {name, isGoodDog, image} = pup
+function DogCard({pup, dogDetail, setDogDetail}) {
+
+    function handleGoodDog(pup) {
+        fetch(`http://localhost:3001/pups/${pup.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({isGoodDog: !pup.isGoodDog})
+        })
+        .then(r => r.json())
+        .then(pup => handleDetail(pup))
+    }
+
+    function handleDetail(pup) {
+       setDogDetail(
+        <div>
+            <img src = {pup.image} alt="pup image"></img>
+            <h3>{pup.name}</h3>
+            <button onClick={() => handleGoodDog(pup)}>{pup.isGoodDog ? 'Good Dog' : 'Bad Dog'}</button>
+        </div>
+       )
+    }
 
     return(
-       <span>{name}</span>
+       <span onClick={() => handleDetail(pup)}>{pup.name}</span>
     )
 }
 
